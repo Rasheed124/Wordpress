48279684b91d463b29754f59408c9db5ce391be4
Rasheed124 <rashdevatrealglobe@gmail.com>
2023-11-06 01:04:51 -1100

    Duro Theme uploaded

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,5 +1,22 @@
-// Page back to top button
 
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+const headerOverlay = document.querySelector(".header.show-mobile-menu::before");
+
+// Toggle mobile menu on hamburger button click
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+
+// Close mobile menu on close button click
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+// Close mobile menu on close button click
+headerOverlay.addEventListener("click", () => hamburgerBtn.click());
+
+
+
+
+
+// Page back to top button
 const toTopBtn = document.getElementById("to-top-button");
 
 if (toTopBtn) {
