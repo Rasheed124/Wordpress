9eba77c92913dcf744013dd92974c50911219bd9
Rasheed124 <rashdevatrealglobe@gmail.com>
2023-11-09 06:37:33 -1100

    duro-theme

--- theme/single.php
+++ theme/single.php
@@ -875,29 +875,6 @@
         <?php endif; ?>
 
 
-<?php if ( have_rows( 'count_down_section' ) ) : ?>
-    <?php while ( have_rows( 'count_down_section' ) ) : the_row(); 
-
-        // Get count_down_id
-        $count_down_id = get_sub_field('count_down_id');
-        if ($count_down_id) { ?>
-
-            <!-- Count Down Section -->
-            <section id="<?php if ( $count_down_id = get_sub_field( 'count_down_id' ) ) : ?><?php echo esc_html( $count_down_id ); ?><?php endif; ?>" class="px-6 lg:px-10 overflow-x-hidden bg-gray-100">
-                <div class="text-center lg:max-w-7xl mx-auto max-w-2xl py-14 lg:py-20">
-
-                    <!-- Display count_down content -->
-                    <?php if ( $count_down = get_sub_field( 'count_down' ) ) : ?>
-                        <?php echo $count_down; ?>
-                    <?php endif; ?>
-
-                </div>
-            </section>
-
-        <?php
-        } // End if $count_down_id exists
-    endwhile; ?>
-<?php endif; ?>
 
 
 
--- theme/template-parts/layout/header-landingpage.php
+++ theme/template-parts/layout/header-landingpage.php
@@ -10,18 +10,18 @@
 ?>
 
 <!-- Single Post Header [Landing Page] -->
-<header>
+<header class="  w-full shadow-sm   font-Antonio  py-5  z-50  bg-white fixed  top-0 text-black px-4">
     <div class="fixed top-0 left-0 w-full h-2 bg-gray-300 z-50">
       <div class="h-full bg-header-dark-overlay" id="readingProgress" style="width: 0;"></div>
-  </div>
+   </div>
 
-      <nav >
-        <div class="w-full shadow-sm h-20 lg:h-[12vh] font-Antonio  sticky top-0 z-50  bg-white  text-black px-4">
+      <nav class="py-2 " >
+ 
 
           <div class="px-4 max-w-7xl h-full mx-auto py-1  flex items-center justify-between ">
 
             <a href="<?php echo get_home_url();?>" class="text-2xl font-Antonio" >
-              <div>
+           
 
 					<?php
 								$custom_logo_id = get_theme_mod( 'custom_logo' );
@@ -32,11 +32,8 @@
 						echo '<h1>' . get_bloginfo('name') . '</h1>';
 					}
 						?>
-              </div>
+          
             </a>
-
-             <div class=" ">
-       
 
                  <ul class="menu   text-lg text-black hidden lg:block">
                  <!-- <ul class="menu   text-base lg:text-lg gap-7 transform translate-y-[-1000px] transition-transform duration-500 absolute  w-full h-auto top-10 left-0  pt-6  px-6 lg:static lg:top-0 lg:py-0   lg:left-0 lg:flex lg:flex-row lg:justify-end  lg:-translate-y-0 lg:bg-transparent  bg-black shadow-lg lg:shadow-none  lg:space-y-0 text-white lg:text-black"> -->
@@ -55,9 +52,6 @@
                     ?>
                  
                 </ul>
-        
-            </div>
-
 
             <div class="harmbuger hidden  w-6 h-5  flex-col justify-between items-center text-4xl text-white cursor-pointer overflow-hidden group">
               <span class="w-full h-[2px] bg-black inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
@@ -65,13 +59,6 @@
               <span class="w-full h-[2px] bg-black inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
             </div>
 
-  
-
-
-      
-
-
-          </div>
         </div>
       </nav>
 
